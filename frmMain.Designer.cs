bd8055a124a2ffd4e640a727660a9da15774d98a
AnasNaamneh <naamnih.anas@gmail.com>
2024-06-23 01:15:39 -0800

    Change Desktop Wallpaper using Win32 API

---
+++ frmMain.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace System_Wizard
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbOptions = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // cbOptions
+            // 
+            this.cbOptions.FormattingEnabled = true;
+            this.cbOptions.Items.AddRange(new object[] {
+            "None",
+            "Change Desktop Wallpaper",
+            "Shut Down"});
+            this.cbOptions.Location = new System.Drawing.Point(39, 55);
+            this.cbOptions.Name = "cbOptions";
+            this.cbOptions.Size = new System.Drawing.Size(200, 24);
+            this.cbOptions.TabIndex = 0;
+            this.cbOptions.SelectedIndexChanged += new System.EventHandler(this.cbOptions_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbOptions);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbOptions;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System_Wizard
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // Change Desktop Wallpaper
+        //-------------------------------------------------------
+        private const int SPI_SETDESKWALLPAPER = 0x0014;
+        private const int SPIF_UPDATEINIFILE = 0x01;
+        private const int SPIF_SENDCHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        //-------------------------------------------------------
+
+        private void cbOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(cbOptions.Text)
+            {
+                case "Change Desktop Wallpaper":
+                    string Wallpaper = GetImagePath();
+                    if (Wallpaper != "")
+                    {
+                    SetWallpaper(Wallpaper);
+                    MessageBox.Show("Desktop Wallpaper has been Changed");
+                    } else
+                        MessageBox.Show("Desktop Wallpaper has not been Changed");
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        private string GetImagePath()
+        {
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+
+                return path;
+            }
+            return "";
+        }
+        public static void SetWallpaper(string imagePath)
+        {
+            if (imagePath == "")
+            {
+                return;
+            }
+            // Set the wallpaper using SystemParametersInfo
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cbOptions.SelectedIndex = 0;
+        }
+    }
+}
