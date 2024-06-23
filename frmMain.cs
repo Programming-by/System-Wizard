0b1721429863cdbbbf700bd57bc806380f03eeba
AnasNaamneh <naamnih.anas@gmail.com>
2024-06-23 01:34:32 -0800

    Shut Down Windows using Win32 API

--- frmMain.cs
+++ frmMain.cs
@@ -26,7 +26,47 @@ namespace System_Wizard
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+       
+        // Shut Down
         //-------------------------------------------------------
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct TokPriv1Luid
+        {
+            public int Count;
+            public long Luid;
+            public int Attr;
+        }
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        internal static extern IntPtr GetCurrentProcess();
+
+        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+        internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr
+        phtok);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        internal static extern bool LookupPrivilegeValue(string host, string name,
+        ref long pluid);
+
+        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+        internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall,
+        ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
+
+        [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
+        internal static extern bool ExitWindowsEx(int flg, int rea);
+
+        internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
+        internal const int TOKEN_QUERY = 0x00000008;
+        internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+        internal const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
+        internal const int EWX_LOGOFF = 0x00000000;
+        internal const int EWX_SHUTDOWN = 0x00000001;
+        internal const int EWX_REBOOT = 0x00000002;
+        internal const int EWX_FORCE = 0x00000004;
+        internal const int EWX_POWEROFF = 0x00000008;
+        internal const int EWX_FORCEIFHUNG = 0x00000010;
+        //-------------------------------------------------------
+
 
         private void cbOptions_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -39,12 +79,19 @@ namespace System_Wizard
                     SetWallpaper(Wallpaper);
                     MessageBox.Show("Desktop Wallpaper has been Changed");
                     } else
-                        MessageBox.Show("Desktop Wallpaper has not been Changed");
+                    MessageBox.Show("Desktop Wallpaper has not been Changed");
+                    break;
 
+                case "Shut Down":
+                    DoExitWin(EWX_SHUTDOWN | EWX_FORCE);
                     break;
                 default:
                     break;
             }
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cbOptions.SelectedIndex = 0;
         }
         private string GetImagePath()
         {
@@ -69,10 +116,22 @@ namespace System_Wizard
             // Set the wallpaper using SystemParametersInfo
             SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
         }
-
-        private void frmMain_Load(object sender, EventArgs e)
+        private void DoExitWin(int flg)
         {
-            cbOptions.SelectedIndex = 0;
+            bool ok;
+            TokPriv1Luid tp;
+            IntPtr hproc = GetCurrentProcess();
+            IntPtr htok = IntPtr.Zero;
+            ok = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
+            tp.Count = 1;
+            tp.Luid = 0;
+            tp.Attr = SE_PRIVILEGE_ENABLED;
+            ok = LookupPrivilegeValue(null, SE_SHUTDOWN_NAME, ref tp.Luid);
+            ok = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+            ok = ExitWindowsEx(flg, 0);
         }
+
+
+
     }
 }
