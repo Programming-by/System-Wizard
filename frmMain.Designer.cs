261d285ad1859ced1d0841ae0270e1dc74e68c11
AnasNaamneh <naamnih.anas@gmail.com>
2024-06-30 10:54:13 -0800

    Get Screen Resolution By Win32 API

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.cbOptions = new System.Windows.Forms.ComboBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblScreenResolution = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDisplayResolution = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbOptions
@@ -49,15 +53,46 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // lblScreenResolution
+            // 
+            this.lblScreenResolution.AutoSize = true;
+            this.lblScreenResolution.Location = new System.Drawing.Point(135, 32);
+            this.lblScreenResolution.Name = "lblScreenResolution";
+            this.lblScreenResolution.Size = new System.Drawing.Size(117, 16);
+            this.lblScreenResolution.TabIndex = 1;
+            this.lblScreenResolution.Text = "Screen Resolution";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDisplayResolution);
+            this.groupBox1.Controls.Add(this.lblScreenResolution);
+            this.groupBox1.Location = new System.Drawing.Point(456, 217);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(263, 190);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblDisplayResolution
+            // 
+            this.lblDisplayResolution.AutoSize = true;
+            this.lblDisplayResolution.Location = new System.Drawing.Point(6, 32);
+            this.lblDisplayResolution.Name = "lblDisplayResolution";
+            this.lblDisplayResolution.Size = new System.Drawing.Size(123, 16);
+            this.lblDisplayResolution.TabIndex = 2;
+            this.lblDisplayResolution.Text = "Display Resolution:";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.cbOptions);
             this.Name = "frmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -66,6 +101,9 @@
 
         private System.Windows.Forms.ComboBox cbOptions;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblScreenResolution;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblDisplayResolution;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -76,7 +76,38 @@ namespace System_Wizard
         internal const uint MB_ICONINFORMATION = 0x00000040;
         internal const uint MB_ICONSTOP = 0x00000010;
 
+        //Screen Resolution
         //-------------------------------------------------------
+        [DllImport("user32.dll")]
+        static extern bool EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+        [StructLayout(LayoutKind.Sequential)]
+        struct DEVMODE
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public int dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+        }
+
+        //-------------------------------------------------------
+
 
 
         private void cbOptions_SelectedIndexChanged(object sender, EventArgs e)
@@ -104,9 +135,21 @@ namespace System_Wizard
                     break;
             }
         }
+       
+        private string GetScreenResolution()
+        {
+            const int ENUM_CURRENT_SETTINGS = -1;
+            DEVMODE devMode;
+            devMode = default;
+            devMode.dmSize = (short)Marshal.SizeOf(devMode);
+            EnumDisplaySettings(null, ENUM_CURRENT_SETTINGS, ref devMode);
+            return $"{devMode.dmPelsWidth} x {devMode.dmPelsHeight}";
+
+        }
         private void frmMain_Load(object sender, EventArgs e)
         {
             cbOptions.SelectedIndex = 0;
+            lblScreenResolution.Text = GetScreenResolution();
         }
         private string GetImagePath()
         {
